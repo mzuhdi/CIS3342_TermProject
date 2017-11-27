0536f99216d4352276698e7bb3dd66b0f4ea76b2
munjalmahida <32910851+munjalmahida@users.noreply.github.com>
2017-11-27 16:27:22 -0500

    ManageStudents Page

---
+++ TermProject/ManageStudents.aspx
@@ -0,0 +1,65 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ManageStudents.aspx.cs" Inherits="TermProject.ManageStudents" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <h1>Manage Students</h1>
+
+        </div>
+        <h2>Studnets Enrolled</h2>
+        <p>
+            <asp:GridView ID="GridView1" runat="server">
+            </asp:GridView>
+            <asp:Label ID="lblStudentError" runat="server"></asp:Label>
+        </p>
+        <p>
+            <asp:Button ID="btnAddStudents" runat="server" Text="Add Students" />
+            &nbsp;&nbsp;&nbsp;
+            <asp:Button ID="btnCancel" runat="server" Text="Cancel" />
+        </p>
+        <asp:Panel ID="PanelAddStudents" runat="server">
+            <h2>Add Student(s)</h2>
+            <p>
+                Major:
+                <asp:DropDownList ID="ddlMajor" runat="server" OnSelectedIndexChanged="ddlMajor_SelectedIndexChanged">
+                </asp:DropDownList>
+                &nbsp;&nbsp;&nbsp; Username:
+                <asp:TextBox ID="txtUserName" runat="server"></asp:TextBox>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <asp:Button ID="btnSearch" runat="server" Text="Search" />
+                &nbsp;&nbsp;&nbsp;
+                <asp:Label ID="lblSearchError" runat="server"></asp:Label>
+            </p>
+            <asp:GridView ID="gvSearch" runat="server" AutoGenerateColumns="false" DataKeyNames="StudentID">
+                <Columns>
+                    <asp:TemplateField HeaderText="Select">
+                         <ItemTemplate>
+                            <asp:CheckBox ID="chkSelect" runat="server" />
+                         </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:BoundField DataField="Username" HeaderText="Username" />
+                    <asp:BoundField DataField="FirstName" HeaderText="First Name" />
+                    <asp:BoundField DataField="LastName" HeaderText="Last Name" />
+                    <asp:BoundField DataField="Major" HeaderText="Major" />
+                    <asp:BoundField DataField="StudentID" HeaderText="StudentID" />
+                </Columns>
+            </asp:GridView>
+            <br />
+            <asp:Button ID="btnAdd" runat="server" OnClick="btnAdd_Click" Text="Add" />
+            &nbsp;&nbsp;&nbsp;
+            <asp:Button ID="btnClose" runat="server" Text="Close" />
+            <br />
+            <h2>&nbsp;</h2>
+        </asp:Panel>
+        <p>
+        </p>
+    </form>
+</body>
+</html>
---
+++ TermProject/ManageStudents.aspx.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using Utilities;
+using System.Data.SqlClient;
+using System.Collections;
+
+namespace TermProject
+{
+    public partial class ManageStudents : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (!IsPostBack)
+            {
+                DBConnect objDB = new DBConnect();
+                String strSQL = "SELECT DISTINCT Major FROM TP_Student";
+                ddlMajor.DataSource = objDB.GetDataSet(strSQL);
+                ddlMajor.DataTextField = "Major";
+                ddlMajor.DataValueField = "Major";
+                ddlMajor.DataBind();
+
+                popStudents();
+            }
+        }
+
+        public void popStudents()
+        {
+            //DataSet myDS = populateByMajor(major);
+            //gvSearch.DataSource =  myDS;
+            //gvSearch.DataBind();
+            DBConnect objDB = new DBConnect();
+
+            string strSQL = "SELECT * FROM dbo.TP_Student";
+            gvSearch.DataSource = objDB.GetDataSet(strSQL);
+            gvSearch.DataBind();
+
+        }
+
+        public void popByMajor(string major)
+        {
+            DataSet myDS = populateByMajor(major); // pxy.searchAccounts(txtSearch.Text);
+            gvSearch.DataSource = myDS;
+            gvSearch.DataBind();
+        }
+
+        public DataSet populateByMajor(string major)
+        {
+            DBConnect objDB = new DBConnect();
+            SqlCommand objCommand = new SqlCommand();
+
+            //objCommand.Parameters.Clear();
+
+            objCommand.CommandType = CommandType.StoredProcedure;
+            objCommand.CommandText = "PopulateStudentsInCourse";     // identify the name of the stored procedure to execute
+            //SqlParameter inputParameter = new SqlParameter("@major", major);
+            //inputParameter.Direction = ParameterDirection.Input;
+            //inputParameter.SqlDbType = SqlDbType.NVarChar;
+            objCommand.Parameters.AddWithValue("@CourseID", 1);
+            //objCommand.Parameters.Add(inputParameter);
+            return objDB.GetDataSetUsingCmdObj(objCommand);
+        }
+
+        public DataSet populateStudentsInCourse(string major)
+        {
+            DBConnect objDB = new DBConnect();
+            SqlCommand objCommand = new SqlCommand();
+
+            //objCommand.Parameters.Clear();
+
+            objCommand.CommandType = CommandType.StoredProcedure;
+            objCommand.CommandText = "TP_SearchByMajor";     // identify the name of the stored procedure to execute
+            //SqlParameter inputParameter = new SqlParameter("@major", major);
+            //inputParameter.Direction = ParameterDirection.Input;
+            //inputParameter.SqlDbType = SqlDbType.NVarChar;
+            objCommand.Parameters.AddWithValue("@major", major);
+            //objCommand.Parameters.Add(inputParameter);
+            return objDB.GetDataSetUsingCmdObj(objCommand);
+        }
+
+        protected void ddlMajor_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            populateByMajor(ddlMajor.SelectedValue.ToString());
+        }
+
+        public List<string> packageStudents()
+        {
+            List<string> packageStudents = new List<string>();
+
+            //int count = 0;
+            //for (int row = 0; row < gvSearch.Rows.Count; row++)
+            //{
+            //    CheckBox cbox;
+            //    cbox = (CheckBox)gvSearch.Rows[row].FindControl("chkSelect");
+            //    if (cbox.Checked)
+            //    {
+            //        string studentID = "";
+
+            //        studentID = gvSearch.DataKeys[0]["StudentID"].ToString();
+            //        packageStudents.Add(studentID);
+
+            //    }
+            //}
+            //return packageStudents;
+            foreach (GridViewRow row in gvSearch.Rows)
+            {
+                // Access the CheckBox
+                CheckBox cb = (CheckBox)row.FindControl("chkSelect");
+                if (cb != null && cb.Checked)
+                {
+                    string StudentID = gvSearch.DataKeys[0]["StudentID"].ToString();
+                    packageStudents.Add(StudentID);
+                }
+            }
+            return packageStudents;
+        }
+
+        public void addStudentsToCourse(ArrayList arrStudents, string courseID)
+        {
+            for (int i = 0; i < arrStudents.Count; i++)
+            {
+
+                DBConnect objDB = new DBConnect();
+                SqlCommand objCommand = new SqlCommand();
+
+
+                objCommand.CommandType = CommandType.StoredProcedure;
+                objCommand.CommandText = "TP_AddStudentsToCourse";
+
+                objCommand.Parameters.AddWithValue("@StudentID", Convert.ToInt32(arrStudents[i]));
+                objCommand.Parameters.AddWithValue("@CourseID", courseID);//Convert.ToInt32(Session["CourseID"]));
+
+                objDB.DoUpdateUsingCmdObj(objCommand);
+                objCommand.Parameters.Clear();
+            }
+        }
+
+        public string arrayTest(List<string> arrStudents)
+        {
+            string ID = "";
+            for (int i = 0; i < arrStudents.Count; i++)
+            {
+                string Student = "";
+
+                ID = arrStudents[i].ToString() + ", ";
+            }
+            return ID;
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            // addStudentsToCourse(packageStudents(), "1");
+            lblSearchError.Text = arrayTest(packageStudents());
+        }
+    }
+}
